72ba163834ad6cceac92ce8f91f330ea8fd116aa
kirillpotekhin <kirill.potekhin@gmail.com>
2020-12-03 03:15:56 +0700

    Доработал тесты

--- src/store/reducers/user/user.test.js
+++ src/store/reducers/user/user.test.js
@@ -1,12 +1,7 @@
 
 import {user} from "./user";
 import {ActionType} from "../../action";
-import {AuthorizationStatus, APIRoute, AppRoute} from "../../../const";
-import MockAdapter from "axios-mock-adapter";
-import {createAPI} from "../../../services/api";
-import {checkAuth, login} from "../../api-actions";
-
-const api = createAPI(() => {});
+import {AuthorizationStatus} from "../../../const";
 
 it(`Reducer should requireAuthorization`, () => {
   expect(user({
@@ -38,51 +33,4 @@ it(`Reducer should getUserEmail`, () => {
     authorizationStatus: AuthorizationStatus.NO_AUTH,
     email: `Oliver.conner@gmail.com`,
   });
-});
-
-describe(`Async operation work correctly`, () => {
-  it(`Should make a correct API call to /login`, () => {
-    const apiMock = new MockAdapter(api);
-    const dispatch = jest.fn();
-    const offersLoader = checkAuth();
-
-    apiMock
-      .onGet(APIRoute.LOGIN)
-      .reply(200, [{fake: true}]);
-
-    return offersLoader(dispatch, () => {}, api)
-      .then(() => {
-        expect(dispatch).toHaveBeenCalledTimes(1);
-        expect(dispatch).toHaveBeenNthCalledWith(1, {
-          type: ActionType.REQUIRED_AUTHORIZATION,
-          payload: AuthorizationStatus.AUTH,
-        });
-      });
-  });
-
-  it(`Should make a correct API call to /login`, () => {
-    const apiMock = new MockAdapter(api);
-    const dispatch = jest.fn();
-    const fakeUser = {login: `test@test.ru`, password: `123456`};
-    const offersLoader = login(fakeUser);
-
-    apiMock
-      .onPost(APIRoute.LOGIN)
-      .reply(200, [{fake: true}]);
-
-    return offersLoader(dispatch, () => {}, api)
-      .then(() => {
-        expect(dispatch).toHaveBeenCalledTimes(3);
-        expect(dispatch).toHaveBeenNthCalledWith(1, {
-          type: ActionType.REQUIRED_AUTHORIZATION,
-          payload: AuthorizationStatus.AUTH,
-        });
-        expect(dispatch).toHaveBeenNthCalledWith(2, {
-          type: ActionType.REDIRECT_TO_ROUTE,
-          payload: AppRoute.ROOT,
-        });
-
-      });
-  });
-
 });
